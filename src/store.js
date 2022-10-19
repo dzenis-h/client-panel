e42d9d88e0a4f984f0fa418c2ebdad083fbd0d3d
Dzenis <dzenis.hankusic@gmail.com>
2022-10-19 19:11:33 +0200

    Fix Redux Store

--- src/store.js
+++ src/store.js
@@ -64,8 +64,8 @@ const store = createStoreWithFirebase(
   rootReducer,
   initialState,
   compose(
-    reactReduxFirebase(firebase),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    reactReduxFirebase(firebase)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
